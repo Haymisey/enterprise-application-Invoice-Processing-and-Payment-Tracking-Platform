183b7c77a8e3c1fd9ea5d5178f4294afe60763eb
Thitna Damtew <thitnadamtew@gmail.com>
2026-01-09 12:29:48 +0300

    Add integration test for Get Invoices API

--- tests/IntegrationTests/InvoiceApiTests.cs
+++ tests/IntegrationTests/InvoiceApiTests.cs
@@ -1,1 +1,29 @@
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
 
+namespace IntegrationTests;
+
+public class InvoiceApiTests : IClassFixture<WebApplicationFactory<InvoiceProcessingPlatform.API.Program>>
+{
+    private readonly WebApplicationFactory<InvoiceProcessingPlatform.API.Program> _factory;
+
+    public InvoiceApiTests(WebApplicationFactory<InvoiceProcessingPlatform.API.Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_Invoices_ReturnsOk()
+    {
+        
+        var client = _factory.CreateClient();
+
+        
+        var response = await client.GetAsync("/api/invoices");
+
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
